728dcfa81866d40e18ab1e19cd26561b5f4095e6
Atish Jain <atish_jain@yahoo.com>
2024-06-06 11:55:00 +0530

    Add files via upload

---
+++ 001_welcome.c
@@ -0,0 +1,17 @@
+/*
+Program name: 001_welcome.c
+Objective: Displays Welcome Message
+Author: Atish Jain
+Date: 6th June 24
+Company: AH CAREER ACADEMY
+*/
+# include <stdio.h>
+main()  //indicates starting point of the program execution
+{
+    printf("Welcome to AH CAREER ACADEMY");
+    printf("\nRajahmundry");
+    printf("\n");
+}
+
+
+
